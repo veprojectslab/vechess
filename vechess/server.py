89d81d5907283fc936c68e7599f8fa7ebfc247e4
Mikhail Efremov <meechanic.design@gmail.com>
2020-08-05 20:34:04 +0300

    Simple GET requests processing added

--- vechess/server.py
+++ vechess/server.py
@@ -2,5 +2,15 @@ import vechess.core as vech
 import json
 
 def app(env, start_response):
-    start_response('200 OK', [('Content-Type','application/json')])
-    return [json.dumps(vech.BoardState_data(), sort_keys=True)]
+    request_method = env["REQUEST_METHOD"]
+    query_string = env["QUERY_STRING"]
+    if request_method == "GET":
+        if query_string == "/state/":
+            start_response('200 OK', [('Content-Type','application/json')])
+            return [json.dumps(vech.BoardState_data(), sort_keys=True)]
+        elif query_string == "/init/":
+            vech.initialize()
+            start_response('200 OK', [('Content-Type','application/json')])
+            return [json.dumps({"response": "OK"})]
+    start_response('501 Not Implemented', [('Content-Type','application/json')])
+    return [json.dumps({"response": "Not implemented"})]
